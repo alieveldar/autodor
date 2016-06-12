04f6343443e5d6bd8899b85743ebda78ed8105c2
modus <illuminatiday@gmail.com>
2016-06-13 01:03:15 +0300

    #017 Fix past authorization in login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,15 @@ class SessionsController < ApplicationController
 		@users = User.all
 	end
 	def new
+    if session[:user_id] != nil
+        destroy
+      end
 
   	end 
 
   def create
+    
+
     puts params[:login]
     user = User.where(login:params[:session][:login]).first
     if user && user.authenticate(params[:session][:password])
