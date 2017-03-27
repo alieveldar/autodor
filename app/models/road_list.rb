07092922a5e1693505bd3bd0e9869bf480445717
alieveldar <illuminatiday@gmail.com>
2017-03-27 16:22:30 +0400

    annotate' '

--- app/models/road_list.rb
+++ app/models/road_list.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: road_lists
+#
+#  id         :integer          not null, primary key
+#  date       :date
+#  timeout    :text
+#  timein     :text
+#  fuelout    :integer
+#  fuelin     :integer
+#  cardcredit :integer
+#  fuelnorm   :integer
+#  fuelfakt   :integer
+#  odometr    :integer
+#  maping     :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class RoadList < ActiveRecord::Base
 end
--- test/models/road_list_test.rb
+++ test/models/road_list_test.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: road_lists
+#
+#  id         :integer          not null, primary key
+#  date       :date
+#  timeout    :text
+#  timein     :text
+#  fuelout    :integer
+#  fuelin     :integer
+#  cardcredit :integer
+#  fuelnorm   :integer
+#  fuelfakt   :integer
+#  odometr    :integer
+#  maping     :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class RoadListTest < ActiveSupport::TestCase
