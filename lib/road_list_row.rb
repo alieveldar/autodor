96649f909ddad8b62c4598585dd95c633c35fe7d
modus <illuminatiday@gmail.com>
2017-07-13 22:03:37 +0300

    #689 Add module month

--- lib/road_list_row.rb
+++ lib/road_list_row.rb
@@ -1,13 +1,13 @@
 class RoadListRow
 
-	attr_accessor :date, :fuel_on_out, :fuel_on_in, :fuel_get_from_card, :distance, :objects, :in_out_odometr, :in_come_back_odometr, :fuel_lost_in_norm, :fuel_lost_in_fact
+	attr_accessor :date, :fuel_on_out, :fuel_on_in, :fuel_get_from_card, :objects, :in_out_odometr, :in_come_back_odometr, :fuel_lost_in_norm, :fuel_lost_in_fact, :distance, :destinations
 	
 	@date
 	@fuel_on_out
 	@fuel_on_in
 	@fuel_get_from_card
 	@distance
-	@objects
+	@destinations
 	@in_out_odometr
 	@in_come_back_odometr
 	@fuel_lost_in_norm
