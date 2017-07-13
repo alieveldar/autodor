892a79cf02e2c73ee13550d750ac4d1dbe67c3cb
modus <illuminatiday@gmail.com>
2017-07-13 21:28:06 +0300

    #683 FIXATOR

--- lib/road_list_row.rb
+++ lib/road_list_row.rb
@@ -1,6 +1,7 @@
 class RoadListRow
 
 	attr_accessor :date, :fuel_on_out, :fuel_on_in, :fuel_get_from_card, :distance, :objects, :in_out_odometr, :in_come_back_odometr, :fuel_lost_in_norm, :fuel_lost_in_fact
+	
 	@date
 	@fuel_on_out
 	@fuel_on_in
@@ -11,4 +12,5 @@ class RoadListRow
 	@in_come_back_odometr
 	@fuel_lost_in_norm
 	@fuel_lost_in_fact
+	
 end
